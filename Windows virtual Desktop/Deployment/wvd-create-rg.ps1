2d60fb963b930ff3255afe851e4d9be85702e9c8
witmbayer <61836092+witmbayer@users.noreply.github.com>
2020-03-06 12:11:18 +0100

    Delete wvd-create-rg.ps1

--- Windows virtual Desktop/Deployment/wvd-create-rg.ps1
+++
@@ -1,9 +0,0 @@
-#    Vaiablen
-$ResourceGroup
-$ResourceGroupLocation = "westeurope"
-
-#   Start
-Connect-AzAccount
-New-AzResourceGroup `
-    -Name $ResourceGroup `
-    -Location $ResourceGroupLocation
